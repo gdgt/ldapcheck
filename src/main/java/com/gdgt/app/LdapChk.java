03d3425b7499a756821ee74bd82ea74368a823aa
michalis <gdgt@users.noreply.github.com>
2018-06-25 11:15:46 +0100

    set org.springframework to debug

--- src/main/java/com/gdgt/app/LdapChk.java
+++ src/main/java/com/gdgt/app/LdapChk.java
@@ -1,41 +1,47 @@
 package com.gdgt.app;
 
-import java.io.File;
-import java.util.*;
-import org.apache.commons.configuration.*;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.ldap.authentication.AbstractLdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Michalis Kongtonk
  * Hello LdapChk!
  */
 public class LdapChk {
+    private static final Logger logger = LogManager.getLogger("LdapChk");
 
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws JSONException {
-
         PropertiesConfiguration config = new PropertiesConfiguration();
         AbstractLdapAuthenticationProvider authProvider = null;
         LdapUserDetailsContextMapper ctxMapper = new LdapUserDetailsContextMapper();
         Authentication result = null;
 
-        // "/Users/michalis/Documents/projects/cmldap/cmldap/src/main/resources/ldap-config.properties"
         if (args.length == 0) {
             System.out.println("Properties file missing.");
             System.exit(1);
@@ -52,25 +58,26 @@ public class LdapChk {
         try {
             if (StringUtils.isEmpty(config.getString("LDAP_TYPE"))) {
                 // Attempt AD
-                System.out.println("Using ActiveDirectory authentication with NT domain: " +
+                logger.info("Using ActiveDirectory authentication with NT domain: " +
                         config.getString("NT_DOMAIN"));
                 authProvider = new ActiveDirectoryLdapAuthenticationProvider(
                         config.getString("NT_DOMAIN"), config.getString("LDAP_URL"));
             } else {
                 if (config.getString("LDAP_TYPE").toUpperCase().contentEquals("LDAP")) {
                     // Setup LDAP bind
-                    System.out.println("Using LDAP authentication with properties: \n" +
-                            "LDAP_BIND_DN: " + config.getString("LDAP_BIND_DN") + "\n" +
-                            "LDAP_USER_SEARCH_BASE: " + config.getString("LDAP_USER_SEARCH_BASE") + "\n" +
-                            "LDAP_USER_SEARCH_FILTER: " + config.getString("LDAP_USER_SEARCH_FILTER") + "\n" +
-                            "LDAP_GROUP_SEARCH_BASE: " + config.getString("LDAP_GROUP_SEARCH_BASE") + "\n" +
-                            "LDAP_GROUP_SEARCH_FILTER: " + config.getString("LDAP_GROUP_SEARCH_FILTER"));
+                    logger.info("Using LDAP authentication with properties:" );
+                    logger.info("LDAP_BIND_DN: " + config.getString("LDAP_BIND_DN"));
+                    logger.info("LDAP_USER_SEARCH_BASE: " + config.getString("LDAP_USER_SEARCH_BASE"));
+                    logger.info("LDAP_USER_SEARCH_FILTER: " + config.getString("LDAP_USER_SEARCH_FILTER"));
+                    logger.info("LDAP_GROUP_SEARCH_BASE: " + config.getString("LDAP_GROUP_SEARCH_BASE"));
+                    logger.info("LDAP_GROUP_SEARCH_FILTER: " + config.getString("LDAP_GROUP_SEARCH_FILTER"));
                     LdapContextSource ctxSource = new LdapContextSource();
                     ctxSource.setUrl(config.getString("LDAP_URL"));
                     ctxSource.setUserDn(config.getString("LDAP_BIND_DN"));
                     ctxSource.setPassword(config.getString("LDAP_BIND_PW"));
                     ctxSource.setReferral(config.getString("FOLLOW_REFERRALS"));
                     ctxSource.afterPropertiesSet();
+
 
                     // Attempt bind with LDAP
                     BindAuthenticator bindAuth = new BindAuthenticator(ctxSource);
@@ -111,7 +118,7 @@ public class LdapChk {
             if (authProvider != null) {
                 authProvider.setUserDetailsContextMapper(ctxMapper);
                 result = authProvider.authenticate(authToken);
-                System.out.println("Authenticated: " + result.isAuthenticated());
+                logger.info("Authenticated: " + result.isAuthenticated());
             }
 
         } catch (Exception ex) {
@@ -132,8 +139,8 @@ public class LdapChk {
                 }
             }
             JSONObject item = new JSONObject().put("items", json);
-            System.out.println("Cloudera Manager REST API example below");
-            System.out.println("curl -X PUT -u admin:admin -H 'Content-Type:application/json' -d '"
+            logger.info("Cloudera Manager REST API example below");
+            logger.info("curl -X PUT -u admin:admin -H 'Content-Type:application/json' -d '"
                     + item.toString() + "' http://$(hostname -f):7180/api/v12/cm/config");
         }
     }
--- src/main/java/com/gdgt/app/LdapUserDetailsContextMapper.java
+++ src/main/java/com/gdgt/app/LdapUserDetailsContextMapper.java
@@ -1,12 +1,14 @@
 package com.gdgt.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
 import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,25 +18,28 @@ import java.util.List;
  * Created by michalis on 22/06/2016.
  */
 public class LdapUserDetailsContextMapper implements UserDetailsContextMapper {
-    @Override
-    public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> authorities) {
+  private static final Logger logger = LogManager.getLogger("LdapUserDetailsContextMapper");
 
-        List<GrantedAuthority> mappedAuthorities = new ArrayList<>();
-        UserDetails user = new LdapUserDetailsMapper().mapUserFromContext(ctx, username, authorities);
+  @Override
+  public UserDetails mapUserFromContext(DirContextOperations ctx, String username,
+                                        Collection<? extends GrantedAuthority> authorities) {
 
-        System.out.println("DN: " + ctx.getNameInNamespace());
-        System.out.println("AUTH_USER_NAME: " + user.getUsername());
-        System.out.println("MemberOf: " + user.getAuthorities());
-        System.out.println("Enabled: " + user.isEnabled());
-        System.out.println("Account Not Locked: " + user.isAccountNonLocked());
-        System.out.println("Account Not Expired: " + user.isAccountNonExpired());
-        System.out.println("Credentials Not Expired: " + user.isCredentialsNonExpired());
+    List<GrantedAuthority> mappedAuthorities = new ArrayList<>();
+    UserDetails user = new LdapUserDetailsMapper().mapUserFromContext(ctx, username, authorities);
 
-        return new User(username, "", true, true, true, true, mappedAuthorities);
-    }
+    logger.info("DN: " + ctx.getNameInNamespace());
+    logger.info("AUTH_USER_NAME: " + user.getUsername());
+    logger.info("MemberOf: " + user.getAuthorities());
+    logger.info("Enabled: " + user.isEnabled());
+    logger.info("Account Not Locked: " + user.isAccountNonLocked());
+    logger.info("Account Not Expired: " + user.isAccountNonExpired());
+    logger.info("Credentials Not Expired: " + user.isCredentialsNonExpired());
 
-    @Override
-    public void mapUserToContext(UserDetails user, DirContextAdapter ctx) {
-        throw new UnsupportedOperationException("LdapUserDetailsContextMapper only supports reading from a context");
-    }
+    return new User(username, "", true, true, true, true, mappedAuthorities);
+  }
+
+  @Override
+  public void mapUserToContext(UserDetails user, DirContextAdapter ctx) {
+    throw new UnsupportedOperationException("LdapUserDetailsContextMapper only supports reading from a context");
+  }
 }
